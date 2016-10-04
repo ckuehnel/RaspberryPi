3b7839e719e7931a96b715e5418741cbd65ff1cc
Claus Kühnel <info@ckuehnel.ch>
2016-10-04 13:16:01 +0200

    Add files via upload
    Adding board description and IP addr into boardinfo.sh

---
+++ board.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Describe your board and used CPU between the control sequences \033[7m and \033[0m
+echo -e "\033[7m Raspberry Pi 3 & WD PiDrive 314 GB \033[0m"
--- boardinfo.sh
+++ boardinfo.sh
@@ -1,23 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "============================="
-echo " UDOO Quad Board Information "
-echo "============================="
+echo "========================================================================"
+echo "Board Information"
+echo "========================================================================"
 echo
-echo "--- CPU Info ----------------"
+
+file="board.sh"
+if [ -f "$file" ]
+then
+  source $file
+else
+  echo "No board description avaliable"
+fi
+
+echo
+echo "--- CPU Info -----------------------------------------------------------"
 cat /proc/cpuinfo
 echo
-echo "---Linux Version ------------"
+echo "--- Linux Version ------------------------------------------------------"
 uname -a
 echo
-echo "--- Uptime ------------------"
+echo "--- Uptime -------------------------------------------------------------"
 uptime
 echo
-echo "--- Memory Usage ------------"
-free -m
+echo "--- IP Address ---------------------------------------------------------"
+ip addr show | grep -w "inet 192"
+echo
+echo "--- Memory Usage -------------------------------------------------------"
+free -
+echo
 df -h
 echo
-echo "--- USB ---------------------"
+echo "--- USB ----------------------------------------------------------------"
 lsusb -tv
-
- 
+echo
